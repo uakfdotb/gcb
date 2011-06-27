f7e6c66ff5a9ae8415f11da1bbb641f312d6ce4b
azn_nz@hotmail.com <azn_nz@hotmail.com@35299b14-8a3f-7d2e-6494-bf2248573a8d>
2011-06-27 09:03:06 +0000

    Accidently uploaded wrong file
    git-svn-id: http://gcb.googlecode.com/svn/trunk@61 35299b14-8a3f-7d2e-6494-bf2248573a8d

--- src/gcb/plugin/Plugin.java
+++ src/gcb/plugin/Plugin.java
@@ -2,250 +2,55 @@
  * To change this template, choose Tools | Templates
  * and open the template in the editor.
  */
- 
+
 package gcb.plugin;
 
-import gcb.GCBConfig;
-import gcb.GarenaInterface;
-import gcb.Main;
+import gcb.GarenaListener;
 import gcb.MemberInfo;
-import gcb.bot.SQLThread;
-import java.io.File;
-import java.net.MalformedURLException;
-import java.net.URL;
-import java.net.URLClassLoader;
-import java.util.ArrayList;
-import java.util.Hashtable;
-import org.apache.commons.configuration.ConfigurationException;
-import org.apache.commons.configuration.ConversionException;
-import org.apache.commons.configuration.PropertiesConfiguration;
 
 /**
  *
  * @author wizardus
  */
-public class PluginManager {
-    String pluginPath;
-    ArrayList<Plugin> pluginList;
-    Hashtable<String, ArrayList<Plugin>> register; //function to plugin list
-    Hashtable<String, Plugin> plugins; //class name to plugin
-
-    GarenaInterface garena;
-    SQLThread sql;
-
-    public PluginManager() {
-        pluginList = new ArrayList<Plugin>();
-        register = new Hashtable<String, ArrayList<Plugin>>();
-        plugins = new Hashtable<String, Plugin>();
-
-        pluginPath = GCBConfig.configuration.getString("gcb_plugin_path", "plugin/");
-    }
-
-    public void setGarena(GarenaInterface garena, SQLThread sql) {
-        this.garena = garena;
-        this.sql = sql;
-    }
-
-    //copied from http://faheemsohail.com/2011/01/writing-a-small-plugin-framework-for-your-apps/
-    public void initPlugins() {
-        File filePath = new File(pluginPath);
-        File files[] = filePath.listFiles();
-        Main.println("[PluginManager] Searching through " + files.length + " files for plugins...");
+public class Plugin implements GarenaListener {
+    public void init(PluginManager manager) {
         
-        // Convert File to a URL
-        ClassLoader loader = null;
-
-        try {
-            URL url = filePath.toURI().toURL();
-            URL[] urls = new URL[]{url};
-            loader = new URLClassLoader(urls);
-        } catch(MalformedURLException e) {
-            if(Main.DEBUG) {
-                e.printStackTrace();
-            }
-            
-            Main.println("[PluginManager] Malformed URL: " + e.getLocalizedMessage());
-            return;
-        }
-
-        //Iterate over files in the plugin directory
-
-        for (File file : files) {
-            if (file.isFile() && (file.getName().endsWith(".gcbp.cfg") || file.getName().endsWith(".plugin.cfg"))) {
-                PropertiesConfiguration pluginConfig = null;
-                try {
-                    pluginConfig = new PropertiesConfiguration(file);
-                } catch(ConfigurationException e) {
-                    if(Main.DEBUG) {
-                        e.printStackTrace();
-                    }
-                    
-                    Main.println("[PluginManager] Warning: unable to parse plugin configuration file " + file.getAbsolutePath());
-                }
-
-                String fullyQualifiedName = pluginConfig.getString("classname");
-                File pluginFile = new File(pluginConfig.getString("pluginfile"));
-
-                try {
-                    Class cls = loader.loadClass(fullyQualifiedName);
-                    //add loaded plugin to plugin list
-                    Plugin plugin = (Plugin) cls.newInstance();
-                    pluginList.add(plugin);
-                    plugins.put(fullyQualifiedName, plugin);
-                } catch(Exception e) {
-                    if(Main.DEBUG) {
-                        e.printStackTrace();
-                    }
-                    
-                    Main.println("[PluginManager] Error while initiating " + fullyQualifiedName + " in " + pluginFile + ":" + e.getLocalizedMessage());
-                }
-            } else {
-                //skip folders
-                continue;
-            }
-        }
-
-        Main.println("[PluginManager] " + pluginList.size() + " plugins found!");
-
-        //call init on all plugins
-        for(Plugin plugin : pluginList) {
-            plugin.init(this);
-        }
     }
 
-    public void register(Plugin p, String s) {
-        ArrayList<Plugin> registerList = register.get(s);
-        if(registerList == null) {
-            registerList = new ArrayList<Plugin>();
-            register.put(s, registerList);
-        }
+    public void load() {
 
-        if(!registerList.contains(p)) {
-            registerList.add(p);
-        }
     }
 
-    public void deregister(Plugin p, String s) {
-        ArrayList<Plugin> registerList = register.get(s);
-        if(registerList != null) {
-            registerList.remove(p);
-        }
-    }
-    
-    public void registerDelayed(Plugin p, String argument, long millis) {
-        DelayThread thread = new DelayThread(p, argument, millis);
-        thread.start();
+    public void unload() {
+
     }
 
-    public void registerListener(Plugin p) {
-        garena.registerListener(p);
-    }
-
-    public void deregisterListener(Plugin p) {
-        garena.deregisterListener(p);
-    }
-
-    public void loadPlugins() {
-        try {
-            String[] pluginNames = GCBConfig.configuration.getStringArray("gcb_plugins");
-
-            Main.println("[PluginManager] Loading " + pluginNames.length + " plugins...");
-            for(String pluginName : pluginNames) {
-                if(pluginName.equals("")) {
-                    continue;
-                }
-
-                loadPlugin(pluginName);
-            }
-        } catch(ConversionException e) {
-            //do nothing
-        }
-    }
-
-    public void loadPlugin(String name) {
-        Main.println("[PluginManager] Loading " + name + "...");
-
-        Plugin plugin = plugins.get(name);
-        if(plugin != null) {
-            plugin.load();
-        } else {
-            Main.println("[PluginManager] Load failed: plugin not found!");
-        }
-    }
-
-    public void unloadPlugin(String name) {
-        Plugin plugin = plugins.get(name);
-        if(plugin != null) {
-            plugin.unload();
-        }
-    }
-
+    //called when GChatBot receives a command
     public String onCommand(MemberInfo player, String command, String payload, boolean isRoomAdmin, boolean isBotAdmin, boolean isSafelist) {
-        ArrayList<Plugin> registerList = register.get("onCommand");
-
-        if(registerList != null) {
-            for(Plugin p : registerList) {
-                String response = p.onCommand(player, command, payload, isRoomAdmin, isBotAdmin, isSafelist);
-                if(response != null) {
-                    return response;
-                }
-            }
-        }
-
         return null;
     }
 
+    //called when GarenaInterface disconnects
     public void onDisconnect(int type) {
-        ArrayList<Plugin> registerList = register.get("onDisconnect");
-
-        if(registerList != null) {
-            for(Plugin p : registerList) {
-                p.onDisconnect(type);
-            }
-        }
+        
     }
 
+    //called when packet is received; type is GARENA_MAIN, GARENA_ROOM, or GARENA_PEER
+    //identifier will be -1 except for GARENA_MAIN
     public void onPacket(int type, int identifier, byte[] bytes, int offset, int length) {
-        ArrayList<Plugin> registerList = register.get("onPacket");
-
-        if(registerList != null) {
-            for(Plugin p : registerList) {
-                p.onPacket(type, identifier, bytes, offset, length);
-            }
-        }
+        
     }
 
-    public void log(String message) {
-        Main.println(message);
+    //called after plugin requests delayed callback
+    public void onDelay(String argument) {
+        
     }
 
-    public GarenaInterface getGarenaInterface() {
-        return garena;
-    }
-
-    public SQLThread getSQLThread() {
-        return sql;
-    }
-}
-
-class DelayThread extends Thread {
-    long millis; //milliseconds to sleep
-    Plugin plugin; //plugin to call
-    String argument;
-
-    public DelayThread(Plugin plugin, String argument, long millis) {
-        this.plugin = plugin;
-        this.argument = argument;
-        this.millis = millis;
-    }
-
-    public void run() {
-        try {
-            Thread.sleep(millis);
-        } catch(InterruptedException ie) {
-
-        }
-
-        plugin.onDelay(argument);
-    }
+    //GarenaListener functions
+    public void chatReceived(MemberInfo player, String text, boolean whisper) {}
+    public void playerJoined(MemberInfo player) {}
+    public void playerLeft(MemberInfo player) {}
+    public void playerStarted(MemberInfo player) {}
+    public void playerStopped(MemberInfo player) {}
+    public void disconnected(int type) {}
 }
--- src/gcb/plugin/PluginManager.java
+++ src/gcb/plugin/PluginManager.java
@@ -2,6 +2,7 @@
  * To change this template, choose Tools | Templates
  * and open the template in the editor.
  */
+ 
 package gcb.plugin;
 
 import gcb.GCBConfig;
@@ -24,227 +25,227 @@ import org.apache.commons.configuration.PropertiesConfiguration;
  * @author wizardus
  */
 public class PluginManager {
-	String pluginPath;
-	ArrayList<Plugin> pluginList;
-	Hashtable<String, ArrayList<Plugin>> register; //function to plugin list
-	Hashtable<String, Plugin> plugins; //class name to plugin
+    String pluginPath;
+    ArrayList<Plugin> pluginList;
+    Hashtable<String, ArrayList<Plugin>> register; //function to plugin list
+    Hashtable<String, Plugin> plugins; //class name to plugin
 
-	GarenaInterface garena;
-	SQLThread sql;
+    GarenaInterface garena;
+    SQLThread sql;
 
-	public PluginManager() {
-		pluginList = new ArrayList<Plugin>();
-		register = new Hashtable<String, ArrayList<Plugin>>();
-		plugins = new Hashtable<String, Plugin>();
+    public PluginManager() {
+        pluginList = new ArrayList<Plugin>();
+        register = new Hashtable<String, ArrayList<Plugin>>();
+        plugins = new Hashtable<String, Plugin>();
 
-		pluginPath = GCBConfig.configuration.getString("gcb_plugin_path", "plugins/");
-	}
+        pluginPath = GCBConfig.configuration.getString("gcb_plugin_path", "plugin/");
+    }
 
-	public void setGarena(GarenaInterface garena, SQLThread sql) {
-		this.garena = garena;
-		this.sql = sql;
-	}
+    public void setGarena(GarenaInterface garena, SQLThread sql) {
+        this.garena = garena;
+        this.sql = sql;
+    }
 
-	//copied from http://faheemsohail.com/2011/01/writing-a-small-plugin-framework-for-your-apps/
-	public void initPlugins() {
-		File filePath = new File(pluginPath);
-		File files[] = filePath.listFiles();
-		Main.println("[PluginManager] Searching through " + files.length + " files for plugins...");
-		
-		// Convert File to a URL
-		ClassLoader loader = null;
+    //copied from http://faheemsohail.com/2011/01/writing-a-small-plugin-framework-for-your-apps/
+    public void initPlugins() {
+        File filePath = new File(pluginPath);
+        File files[] = filePath.listFiles();
+        Main.println("[PluginManager] Searching through " + files.length + " files for plugins...");
+        
+        // Convert File to a URL
+        ClassLoader loader = null;
 
-		try {
-			URL url = filePath.toURI().toURL();
-			URL[] urls = new URL[]{url};
-			loader = new URLClassLoader(urls);
-		} catch(MalformedURLException e) {
-			if(Main.DEBUG) {
-				e.printStackTrace();
-			}
-			
-			Main.println("[PluginManager] Malformed URL: " + e.getLocalizedMessage());
-			return;
-		}
+        try {
+            URL url = filePath.toURI().toURL();
+            URL[] urls = new URL[]{url};
+            loader = new URLClassLoader(urls);
+        } catch(MalformedURLException e) {
+            if(Main.DEBUG) {
+                e.printStackTrace();
+            }
+            
+            Main.println("[PluginManager] Malformed URL: " + e.getLocalizedMessage());
+            return;
+        }
 
-		//Iterate over files in the plugin directory
+        //Iterate over files in the plugin directory
 
-		for (File file : files) {
-			if (file.isFile() && (file.getName().endsWith(".gcbp.cfg") || file.getName().endsWith(".plugin.cfg"))) {
-				PropertiesConfiguration pluginConfig = null;
-				try {
-					pluginConfig = new PropertiesConfiguration(file);
-				} catch(ConfigurationException e) {
-					if(Main.DEBUG) {
-						e.printStackTrace();
-					}
-					
-					Main.println("[PluginManager] Warning: unable to parse plugin configuration file " + file.getAbsolutePath());
-				}
+        for (File file : files) {
+            if (file.isFile() && (file.getName().endsWith(".gcbp.cfg") || file.getName().endsWith(".plugin.cfg"))) {
+                PropertiesConfiguration pluginConfig = null;
+                try {
+                    pluginConfig = new PropertiesConfiguration(file);
+                } catch(ConfigurationException e) {
+                    if(Main.DEBUG) {
+                        e.printStackTrace();
+                    }
+                    
+                    Main.println("[PluginManager] Warning: unable to parse plugin configuration file " + file.getAbsolutePath());
+                }
 
-				String fullyQualifiedName = pluginConfig.getString("classname");
-				File pluginFile = new File(pluginConfig.getString("pluginfile"));
+                String fullyQualifiedName = pluginConfig.getString("classname");
+                File pluginFile = new File(pluginConfig.getString("pluginfile"));
 
-				try {
-					Class cls = loader.loadClass(fullyQualifiedName);
-					//add loaded plugin to plugin list
-					Plugin plugin = (Plugin) cls.newInstance();
-					pluginList.add(plugin);
-					plugins.put(fullyQualifiedName, plugin);
-				} catch(Exception e) {
-					if(Main.DEBUG) {
-						e.printStackTrace();
-					}
-					
-					Main.println("[PluginManager] Error while initiating " + fullyQualifiedName + " in " + pluginFile + ":" + e.getLocalizedMessage());
-				}
-			} else {
-				//skip folders
-				continue;
-			}
-		}
+                try {
+                    Class cls = loader.loadClass(fullyQualifiedName);
+                    //add loaded plugin to plugin list
+                    Plugin plugin = (Plugin) cls.newInstance();
+                    pluginList.add(plugin);
+                    plugins.put(fullyQualifiedName, plugin);
+                } catch(Exception e) {
+                    if(Main.DEBUG) {
+                        e.printStackTrace();
+                    }
+                    
+                    Main.println("[PluginManager] Error while initiating " + fullyQualifiedName + " in " + pluginFile + ":" + e.getLocalizedMessage());
+                }
+            } else {
+                //skip folders
+                continue;
+            }
+        }
 
-		Main.println("[PluginManager] " + pluginList.size() + " plugins found!");
+        Main.println("[PluginManager] " + pluginList.size() + " plugins found!");
 
-		//call init on all plugins
-		for(Plugin plugin : pluginList) {
-			plugin.init(this);
-		}
-	}
+        //call init on all plugins
+        for(Plugin plugin : pluginList) {
+            plugin.init(this);
+        }
+    }
 
-	public void register(Plugin p, String s) {
-		ArrayList<Plugin> registerList = register.get(s);
-		if(registerList == null) {
-			registerList = new ArrayList<Plugin>();
-			register.put(s, registerList);
-		}
+    public void register(Plugin p, String s) {
+        ArrayList<Plugin> registerList = register.get(s);
+        if(registerList == null) {
+            registerList = new ArrayList<Plugin>();
+            register.put(s, registerList);
+        }
 
-		if(!registerList.contains(p)) {
-			registerList.add(p);
-		}
-	}
+        if(!registerList.contains(p)) {
+            registerList.add(p);
+        }
+    }
 
-	public void deregister(Plugin p, String s) {
-		ArrayList<Plugin> registerList = register.get(s);
-		if(registerList != null) {
-			registerList.remove(p);
-		}
-	}
-	
-	public void registerDelayed(Plugin p, String argument, long millis) {
-		DelayThread thread = new DelayThread(p, argument, millis);
-		thread.start();
-	}
+    public void deregister(Plugin p, String s) {
+        ArrayList<Plugin> registerList = register.get(s);
+        if(registerList != null) {
+            registerList.remove(p);
+        }
+    }
+    
+    public void registerDelayed(Plugin p, String argument, long millis) {
+        DelayThread thread = new DelayThread(p, argument, millis);
+        thread.start();
+    }
 
-	public void registerListener(Plugin p) {
-		garena.registerListener(p);
-	}
+    public void registerListener(Plugin p) {
+        garena.registerListener(p);
+    }
 
-	public void deregisterListener(Plugin p) {
-		garena.deregisterListener(p);
-	}
+    public void deregisterListener(Plugin p) {
+        garena.deregisterListener(p);
+    }
 
-	public void loadPlugins() {
-		try {
-			String[] pluginNames = GCBConfig.configuration.getStringArray("gcb_plugins");
+    public void loadPlugins() {
+        try {
+            String[] pluginNames = GCBConfig.configuration.getStringArray("gcb_plugins");
 
-			Main.println("[PluginManager] Loading " + pluginNames.length + " plugins...");
-			for(String pluginName : pluginNames) {
-				if(pluginName.equals("")) {
-					continue;
-				}
+            Main.println("[PluginManager] Loading " + pluginNames.length + " plugins...");
+            for(String pluginName : pluginNames) {
+                if(pluginName.equals("")) {
+                    continue;
+                }
 
-				loadPlugin(pluginName);
-			}
-		} catch(ConversionException e) {
-			//do nothing
-		}
-	}
+                loadPlugin(pluginName);
+            }
+        } catch(ConversionException e) {
+            //do nothing
+        }
+    }
 
-	public void loadPlugin(String name) {
-		Main.println("[PluginManager] Loading " + name + "...");
+    public void loadPlugin(String name) {
+        Main.println("[PluginManager] Loading " + name + "...");
 
-		Plugin plugin = plugins.get(name);
-		if(plugin != null) {
-			plugin.load();
-		} else {
-			Main.println("[PluginManager] Load failed: plugin not found!");
-		}
-	}
+        Plugin plugin = plugins.get(name);
+        if(plugin != null) {
+            plugin.load();
+        } else {
+            Main.println("[PluginManager] Load failed: plugin not found!");
+        }
+    }
 
-	public void unloadPlugin(String name) {
-		Plugin plugin = plugins.get(name);
-		if(plugin != null) {
-			plugin.unload();
-		}
-	}
+    public void unloadPlugin(String name) {
+        Plugin plugin = plugins.get(name);
+        if(plugin != null) {
+            plugin.unload();
+        }
+    }
 
-	public String onCommand(MemberInfo player, String command, String payload, boolean isAdmin, boolean isSafelist) {
-		ArrayList<Plugin> registerList = register.get("onCommand");
+    public String onCommand(MemberInfo player, String command, String payload, boolean isRoomAdmin, boolean isBotAdmin, boolean isSafelist) {
+        ArrayList<Plugin> registerList = register.get("onCommand");
 
-		if(registerList != null) {
-			for(Plugin p : registerList) {
-				String response = p.onCommand(player, command, payload, isAdmin, isSafelist);
-				if(response != null) {
-					return response;
-				}
-			}
-		}
+        if(registerList != null) {
+            for(Plugin p : registerList) {
+                String response = p.onCommand(player, command, payload, isRoomAdmin, isBotAdmin, isSafelist);
+                if(response != null) {
+                    return response;
+                }
+            }
+        }
 
-		return null;
-	}
+        return null;
+    }
 
-	public void onDisconnect(int type) {
-		ArrayList<Plugin> registerList = register.get("onDisconnect");
+    public void onDisconnect(int type) {
+        ArrayList<Plugin> registerList = register.get("onDisconnect");
 
-		if(registerList != null) {
-			for(Plugin p : registerList) {
-				p.onDisconnect(type);
-			}
-		}
-	}
+        if(registerList != null) {
+            for(Plugin p : registerList) {
+                p.onDisconnect(type);
+            }
+        }
+    }
 
-	public void onPacket(int type, int identifier, byte[] bytes, int offset, int length) {
-		ArrayList<Plugin> registerList = register.get("onPacket");
+    public void onPacket(int type, int identifier, byte[] bytes, int offset, int length) {
+        ArrayList<Plugin> registerList = register.get("onPacket");
 
-		if(registerList != null) {
-			for(Plugin p : registerList) {
-				p.onPacket(type, identifier, bytes, offset, length);
-			}
-		}
-	}
+        if(registerList != null) {
+            for(Plugin p : registerList) {
+                p.onPacket(type, identifier, bytes, offset, length);
+            }
+        }
+    }
 
-	public void log(String message) {
-		Main.println(message);
-	}
+    public void log(String message) {
+        Main.println(message);
+    }
 
-	public GarenaInterface getGarenaInterface() {
-		return garena;
-	}
+    public GarenaInterface getGarenaInterface() {
+        return garena;
+    }
 
-	public SQLThread getSQLThread() {
-		return sql;
-	}
+    public SQLThread getSQLThread() {
+        return sql;
+    }
 }
 
 class DelayThread extends Thread {
-	long millis; //milliseconds to sleep
-	Plugin plugin; //plugin to call
-	String argument;
+    long millis; //milliseconds to sleep
+    Plugin plugin; //plugin to call
+    String argument;
 
-	public DelayThread(Plugin plugin, String argument, long millis) {
-		this.plugin = plugin;
-		this.argument = argument;
-		this.millis = millis;
-	}
+    public DelayThread(Plugin plugin, String argument, long millis) {
+        this.plugin = plugin;
+        this.argument = argument;
+        this.millis = millis;
+    }
 
-	public void run() {
-		try {
-			Thread.sleep(millis);
-		} catch(InterruptedException ie) {
+    public void run() {
+        try {
+            Thread.sleep(millis);
+        } catch(InterruptedException ie) {
 
-		}
+        }
 
-		plugin.onDelay(argument);
-	}
+        plugin.onDelay(argument);
+    }
 }
