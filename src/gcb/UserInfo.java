f91b721ef3460ea0dd940beae7da398ae177d0e5
azn_nz@hotmail.com <azn_nz@hotmail.com@35299b14-8a3f-7d2e-6494-bf2248573a8d>
2011-09-15 05:41:59 +0000

    Not sure why this wasn't uploaded in previous revision
    git-svn-id: http://gcb.googlecode.com/svn/trunk@125 35299b14-8a3f-7d2e-6494-bf2248573a8d

---
+++ src/gcb/UserInfo.java
@@ -0,0 +1,21 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package gcb;
+
+/**
+ *
+ * @author XIII.Dragon
+ */
+public class UserInfo {
+	public String username;
+	public String properUsername;
+	public int userID;
+	public int rank;
+	public String ipAddress;
+	public String lastSeen;
+	public String promotedBy;
+	public String unbannedBy;
+}
