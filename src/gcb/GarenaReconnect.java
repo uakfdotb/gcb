4e78c6478f30bd76668b846a07a7583356a3c2d8
Jack Lu <jacklu@entgaming.net>
2014-01-21 17:39:01 -0500

    Improve GarenaReconnect by having thread to handle reconnection job requests.
    This thread handles duplicates so that we don't spawn too many reconnection job threads, as could happen before even after numerous fixes.

--- src/gcb/GarenaReconnect.java
+++ src/gcb/GarenaReconnect.java
@@ -5,15 +5,75 @@
 
 package gcb;
 
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Set;
+
 /**
  *
  * @author wizardus
  */
-public class GarenaReconnect implements GarenaListener {
+public class GarenaReconnect extends Thread implements GarenaListener {
 	Main main;
+	List<ReconnectJob> reconnectJobs;
 
 	public GarenaReconnect(Main main) {
 		this.main = main;
+		reconnectJobs = new ArrayList<ReconnectJob>();
+		start();
+	}
+	
+	public void run() {
+		while(true) {
+			synchronized(reconnectJobs) {
+				int sleepTime = 0;
+				
+				//remove duplicate jobs
+				Set<Integer> seenGarena = new HashSet<Integer>();
+				Iterator<ReconnectJob> it = reconnectJobs.iterator();
+				
+				while(it.hasNext()) {
+					ReconnectJob job = it.next();
+					
+					if(seenGarena.contains(job.garena.id)) {
+						it.remove();
+					} else {
+						seenGarena.add(job.garena.id);
+						
+						if(sleepTime == 0 || job.time - System.currentTimeMillis() < sleepTime) {
+							sleepTime = (int) (job.time - System.currentTimeMillis());
+							
+							if(sleepTime == 0) {
+								sleepTime = -1;
+							}
+						}
+					}
+				}
+				
+				//wait for updates or jobs to do
+				if(sleepTime >= 0) {
+					try {
+						reconnectJobs.wait(sleepTime);
+					} catch(InterruptedException ie) {}
+				}
+				
+				//complete jobs passed time
+				it = reconnectJobs.iterator();
+				
+				while(it.hasNext()) {
+					ReconnectJob job = it.next();
+					
+					if(job.time <= System.currentTimeMillis()) {
+						GarenaReconnectThread rt = new GarenaReconnectThread(job.garena, main);
+						rt.start();
+						
+						it.remove();
+					}
+				}
+			}
+		}
 	}
 
 	public void chatReceived(GarenaInterface source, MemberInfo member, String chat, boolean whisper) {}
@@ -33,57 +93,52 @@ public class GarenaReconnect implements GarenaListener {
 			return;
 		}
 
-		GarenaReconnectThread rt = new GarenaReconnectThread(source, main, x);
-		rt.start();
+		synchronized(reconnectJobs) {
+			reconnectJobs.add(new ReconnectJob(source));
+			reconnectJobs.notifyAll();
+		}
+	}
+	
+	class ReconnectJob {
+		GarenaInterface garena;
+		long time;
+		
+		public ReconnectJob(GarenaInterface garena) {
+			this.garena = garena;
+			this.time = System.currentTimeMillis() + 10 * 1000;
+		}
 	}
 }
 
 class GarenaReconnectThread extends Thread {
 	GarenaInterface garena;
 	Main main;
-	int x;
 
-	public GarenaReconnectThread(GarenaInterface garena, Main main, int x) {
+	public GarenaReconnectThread(GarenaInterface garena, Main main) {
 		this.garena = garena;
 		this.main = main;
-		this.x = x;
 	}
 
 	public void run() {
 		synchronized(garena) {
 			if(garena.socket == null || garena.socket.isClosed()) {
-				try {
-					Thread.sleep(10000);
-				} catch(InterruptedException e) {
-		
-				}
-				
 				if(!main.initGarena(garena, true)) {
 					garena.disconnected(GarenaInterface.GARENA_MAIN, true);
+					return;
 				}
 			}
 	
 			if(garena.room_socket == null || garena.room_socket.isClosed()) {
-				try {
-					Thread.sleep(10000);
-				} catch(InterruptedException e) {
-		
-				}
-				
 				if(!main.initRoom(garena, true)) {
 					garena.disconnected(GarenaInterface.GARENA_ROOM, true);
+					return;
 				}
 			}
 	
 			if(garena.peer_socket == null || garena.peer_socket.isClosed()) {
-				try {
-					Thread.sleep(10000);
-				} catch(InterruptedException e) {
-		
-				}
-				
 				if(!main.initPeer(garena, true)) {
 					garena.disconnected(GarenaInterface.GARENA_PEER, true);
+					return;
 				}
 			}
 		}
